b59e2a71e8b66083bc019eddd4c1b0d8e52cf376
Aditi Dubey <aditidubey576@gmail.com>
2023-02-05 18:21:25 +0530

    completed few questions

--- JS Projects/JS Questions/18 Question/solution.js
+++
@@ -1,2 +0,0 @@
-//Write a program which tells the number of days in a month, now consider leap year.
-let new = get date.month 
--- JS Projects/JS Questions/28 Question/sollution.js
+++ JS Projects/JS Questions/28 Question/sollution.js
@@ -1,11 +1,36 @@
 // Printing a Pyramid with the help of Loops 
+
+// A Part (Printing a Triangle )
 let n = 3;
-for (let i = 1; i <= n; i++){
-    for (let j = 1; j <=n; j++){
-        process.stdout.write(' ')
+for (let row = 1; row <= n; row++){
+    for (let col = 1; col <= row; col++){
+document.write("* ");
     }
-    for (let k = 1; k < 2 * i - 1; k++){
-        process.stdout.write(' *')
-    }
-    console.log();
+document.write("<br>");
 }
+
+
+
+// B Part (Printing a Square)
+let a = 3;
+for(let row = 1; row <= a; row++){
+   for(let col = 1; col <= a; col++){
+document.write("* ");
+   }
+   document.write("<br>");
+}
+
+// C Part (Printing a Pyramid)
+let b = 3;
+for(let i = 1; i <= b ; i++){
+   for(let j = 1; j <= b - i; j++){
+      document.write("&nbsp");
+   }
+   for(let k = 1; k <= 2 * i - 1; k++){
+      document.write(" * ");
+   }
+   document.write("<br>"); 
+}
+
+
+
